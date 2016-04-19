a8f7d15c6c45f9c217b14576f8254b4f2bad0e61
tomomifujiwara <the.die.is.cast.19820706@gmail.com>
2016-04-19 23:14:37 +0800

    コード追加

--- dbconnect.php
+++ dbconnect.php
@@ -1,1 +1,9 @@
-dbconnect.php
+<?php
+    // データベース接続処理
+
+  $db = mysqli_connect('localhost', 'root', 'mysql', 'photo_vote')
+  or die(mysqli_connect_error());
+
+  mysqli_set_charset($db, 'utf8');
+
+?>
