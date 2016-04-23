c686d1f264f8cca97763dae5b60fdfe655be34aa
tomomifujiwara <the.die.is.cast.19820706@gmail.com>
2016-04-23 15:43:01 +0800

    コード・コメント追加

--- new.php
+++ new.php
@@ -7,76 +7,76 @@
   $_SESSION['id'] = 1;
   $_SESSION['time'] = time();
 
-      // ログイン判定
-      if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time() ) {
-          $_SESSION['time'] = time();
+    // ログイン判定
+    if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time() ) {
+        $_SESSION['time'] = time();
 
-          $sql = sprintf('SELECT * FROM members WHERE id=%d',
-              mysqli_real_escape_string($db, $_SESSION['id'])
-          );
-          $record = mysqli_query($db, $sql) or die(mysqli_error($db));
+        $sql = sprintf('SELECT * FROM members WHERE id=%d',
+            mysqli_real_escape_string($db, $_SESSION['id'])
+        );
+        $record = mysqli_query($db, $sql) or die(mysqli_error($db));
 
-          // ログインしているのユーザーのデータ
-          $member = mysqli_fetch_assoc($record);
-      } else {
-          // ログインしていない
-          header('Location: signin.php');
-          exit();
-      }
+        // ログインしているのユーザーのデータ
+        $member = mysqli_fetch_assoc($record);
+    } else {
+        // ログインしていない
+        header('Location: signin.php');
+        exit();
+    }
 
-            // 新規投稿セット
-            // 写真の登録
-            $_FILES['image']['name'] = 'phpto_path';
-            $fileName = $_FILES['image']['name'];
+        // 新規投稿セット
+        // 写真の登録
+        $_FILES['image']['name'] = 'phpto_path';
+        $fileName = $_FILES['image']['name'];
 
-                // もしエラーがなければ
-                if (empty($error)) {
-                $image = date("YmdHis") . $_FILES['image']['name'];
-                // move_uploaded_file($_FILES['image']['tmp_name'], '../member_picture/' . $image);
-                }
+        // もしエラーがなければ
+        if (empty($error)) {
+        $image = date("YmdHis") . $_FILES['image']['name'];
+        move_uploaded_file($_FILES['image']['tmp_name'], '../member_picture/' . $image);
+        }
 
-                  // 投稿した際の確認画面が必要か？(seed_snsのcheck.phpとthanks.phpページ的なもの)
-                  // $_SESSION['join'] = $_POST;
-                  // $_SESSION['join']['image'] = $image;
-
-
-                        // 投稿を記録
-                        if (!empty($_POST)) {
-                            if ($_POST['title'] != '') {
-                                $sql = sprintf('INSERT INTO photos SET photo_path=%d , title="%s", comment="%s" member_id=%d , created=NOW()',
-                                mysqli_real_escape_string($db, $_FILES['photo_path']),
-                                mysqli_real_escape_string($db, $_POST['title']),
-                                mysqli_real_escape_string($db, $_POST['comment']),
-                                mysqli_real_escape_string($db, $member['member_id'])
-                                );
-
-                                mysqli_query($db,$sql) or die(mysqli_error($db));
-                            }
-                         }
-
-             // 写真の有無チェック
-
-              // 写真がない場合、「写真が選択されていません」と表示
-              // 写真のエラー文
-
-              // if(!empty($fileName)) {
-              // $ext = substr($fileName, -3);
-              // substr()関数は指定した文字列から指定した数だけ文字を取得する
-              // 今回は-3と指定することで文字列の最後から３つ取得
-              // echo $ext;
-
-              // 画像ファイルの拡張子がjpgもしくはpngでなければtypeというエラーを出す
-              // if ($ext != 'jpg' && $ext != 'png') {
-              // $error['image'] = 'type';
-              // }
-              // }
-
-              // イメージをサーバーにアップロード
+        // セッションのjoinに.$imageに保存されたイメージを呼び出す為に$_POSTの情報を入れる
+          // $_SESSION['join'] = $_POST;
+          // $_SESSION['join']['image'] = $image;
 
 
-              // signin.phpに遷移
-              // header('Location: signin.php');
-              // exit();
+        // 投稿を記録
+        if (!empty($_POST)) {
+            if ($_POST['title'] != '') {
+                  $sql = sprintf('INSERT INTO photos SET photo_path=%d , title="%s", comment="%s" member_id=%d , created=NOW()',
+                  mysqli_real_escape_string($db, $_FILES['photo_path']),
+                  mysqli_real_escape_string($db, $_POST['title']),
+                  mysqli_real_escape_string($db, $_POST['comment']),
+                  mysqli_real_escape_string($db, $member['member_id'])
+                  );
+
+                  mysqli_query($db,$sql) or die(mysqli_error($db));
+            }
+         }
+
+// 写真の有無チェック
+
+      // 写真がない場合、「写真が選択されていません」と表示
+      // 写真のエラー文
+
+      // if(!empty($fileName)) {
+      // $ext = substr($fileName, -3);
+      // substr()関数は指定した文字列から指定した数だけ文字を取得する
+      // 今回は-3と指定することで文字列の最後から３つ取得
+      // echo $ext;
+
+      // 画像ファイルの拡張子がjpgもしくはpngでなければtypeというエラーを出す
+      // if ($ext != 'jpg' && $ext != 'png') {
+      // $error['image'] = 'type';
+      // }
+      // }
+
+      // イメージをサーバーにアップロード
+
+
+      // index.phpに遷移
+      // header('Location: login.php');
+      // exit();
 
 ?>
 
